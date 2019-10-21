01d695636fd334b1ad62923398bbbdff0fd63f78
BrennerBatR <brenner.batista@outlook.com>
2019-10-21 09:21:46 -0300

    CreatePassword esqueleto

---
+++ public/_js/createPassword.js
@@ -0,0 +1,105 @@
+(function ($) {
+    "use strict";
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.criar').on('click', function () {
+        var check = true;
+
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+        if (check) {
+            CreatePassword();
+        } else
+            return check;
+
+    });
+
+
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+        });
+    });
+
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        } else {
+            if ($(input).val().trim() == '') {
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+})(jQuery);
+
+
+
+function  CreatePassword(){ //primeiro login
+    // let url = sessionStorage.getItem("url");
+    // let ra = document.getElementById("username").value;
+    // let pass = document.getElementById("pass");
+    // let pass2 = document.getElementById("pass2");
+
+    // if (VeifyPasswords(pass,pass2)) {
+    //     let password = pass.value;
+    //     $.ajax({
+    //         type: 'GET',
+    //         url: url + '/login?ra='+ra +'&password=' + password,
+    //         success: function (data) {
+
+    //             let json = JSON.parse(data);
+
+    //             alert(json["msg"]);
+    //             if (json['success'] === 2 || json['success'] === -3){
+    //                 window.location.href = 'index.html';
+    //             }else {
+    //                 location.reload();
+    //             }
+
+    //         },
+    //         error: function () {
+    //             alert("Erro na solicitação, tente novamente!")
+    //         }
+    //     });
+    // }
+    // else{
+    //     alert("Senhas não conferem!");
+    // }
+}
+
+/**
+ * @return {boolean}
+ */
+function VeifyPasswords(pass, pass2) {
+    if (pass.value !== pass2.value){
+        pass.value = "";
+        pass2.value = "";
+        pass.focus();
+        return false;
+    }
+    else{
+        return true
+    }
+
+}
